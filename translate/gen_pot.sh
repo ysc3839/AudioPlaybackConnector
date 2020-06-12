4d2d81cf8fcda203ae18b0f43aa9ec3d7ae74c14
Richard Yu <yurichard3839@gmail.com>
2020-06-13 02:58:48 +0800

    Update gen_pot.sh and add xaml pot

--- translate/gen_pot.sh
+++ translate/gen_pot.sh
@@ -5,5 +5,7 @@ FILES="AudioPlaybackConnector.cpp"
 COPYRIGHT_HOLDER="Richard Yu <yurichard3839@gmail.com>"
 PKG_NAME="AudioPlaybackConnector"
 
+pipenv run xaml2po.py ../Menu.xaml xaml/messages.pot
+
 cd ..
 xgettext -o translate/source/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 --copyright-holder="$COPYRIGHT_HOLDER" --package-name="$PKG_NAME" $FILES
