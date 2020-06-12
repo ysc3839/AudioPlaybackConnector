c17827b6b04f80cb219b16a6c4f56ba45fab5541
Richard Yu <yurichard3839@gmail.com>
2020-06-13 03:09:46 +0800

    Add zh_TW to gen_rc.sh

--- translate/gen_rc.sh
+++ translate/gen_rc.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 pipenv run po2ymo.py ./source/zh_CN.po ./generated/zh_CN.ymo
+pipenv run po2ymo.py ./source/zh_TW.po ./generated/zh_TW.ymo
+
 echo '#include "../../targetver.h"' > ./generated/translate.rc
 echo '#include "windows.h"' >> ./generated/translate.rc
+
 echo 'LANGUAGE LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED' >> ./generated/translate.rc
 echo '1 YMO "zh_CN.ymo"' >> ./generated/translate.rc
+
+echo 'LANGUAGE LANG_CHINESE, SUBLANG_CHINESE_TRADITIONAL' >> ./generated/translate.rc
+echo '1 YMO "zh_TW.ymo"' >> ./generated/translate.rc
