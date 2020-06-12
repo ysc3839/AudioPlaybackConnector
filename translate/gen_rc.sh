da39a942f38accae891df930a7b0a4074cdeb7d4
Richard Yu <yurichard3839@gmail.com>
2020-06-13 03:49:30 +0800

    Add po2xaml.py and xamlmin.py, update gen_rc.sh

--- translate/gen_rc.sh
+++ translate/gen_rc.sh
@@ -3,11 +3,20 @@
 pipenv run po2ymo.py ./source/zh_CN.po ./generated/zh_CN.ymo
 pipenv run po2ymo.py ./source/zh_TW.po ./generated/zh_TW.ymo
 
+pipenv run xamlmin.py ../Menu.xaml ./generated/en_US.xaml
+pipenv run po2xaml.py ./xaml/zh_CN.po ./generated/zh_CN.xaml -t ../Menu.xaml
+#pipenv run po2xaml.py ./xaml/zh_TW.po ./generated/zh_TW.xaml -t ../Menu.xaml
+
 echo '#include "../../targetver.h"' > ./generated/translate.rc
 echo '#include "windows.h"' >> ./generated/translate.rc
 
+echo 'LANGUAGE LANG_ENGLISH, SUBLANG_ENGLISH_US' >> ./generated/translate.rc
+echo '1 XAML "en_US.xaml"' >> ./generated/translate.rc
+
 echo 'LANGUAGE LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED' >> ./generated/translate.rc
+echo '1 XAML "zh_CN.xaml"' >> ./generated/translate.rc
 echo '1 YMO "zh_CN.ymo"' >> ./generated/translate.rc
 
 echo 'LANGUAGE LANG_CHINESE, SUBLANG_CHINESE_TRADITIONAL' >> ./generated/translate.rc
+#echo '1 XAML "zh_TW.xaml"' >> ./generated/translate.rc
 echo '1 YMO "zh_TW.ymo"' >> ./generated/translate.rc
---
+++ translate/po2xaml.py
@@ -0,0 +1,41 @@
+from translate.convert import convert
+from translate.storage import po
+from xml.dom.minidom import parse, Node
+
+def po2xaml(node, names):
+    for n in node.childNodes:
+        if n.nodeType == Node.TEXT_NODE and n.nodeValue:
+            n.nodeValue = n.nodeValue.strip()
+        elif n.nodeType == Node.ELEMENT_NODE:
+            if (n.hasAttribute('Text')):
+                n.setAttribute('Text', names[n.getAttribute('Name')])
+            po2xaml(n, names)
+
+def run_converter(inputfile, outputfile, templatefile, includefuzzy=False):
+    source_store = po.pofile(inputfile)
+    names = {}
+    for unit in source_store.units:
+        if unit.istranslated() or (unit.isfuzzy() and includefuzzy and unit.target):
+            loc, = unit.getlocations()
+            tag_name, name = loc.split(':')
+            names[name] = unit.target
+
+    xml = parse(templatefile)
+    po2xaml(xml, names)
+    xml.normalize()
+
+    xml_str = xml.toxml()
+    i = xml_str.find('?>') # remove prefix '<?xml version="1.0" ?>'
+    xml_str = xml_str[i+2:]
+
+    outputfile.write(xml_str.encode('utf-16le'))
+    outputfile.write(bytes(2)) # null terminator
+    return 1
+
+def main(argv=None):
+    formats = {('po', 'xaml'): ('xaml', run_converter)}
+    parser = convert.ConvertOptionParser(formats, usetemplates=True, description=__doc__)
+    parser.run(argv)
+
+if __name__ == '__main__':
+    main()
